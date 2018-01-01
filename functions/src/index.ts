e73e2a5d56038076d814caae4586b76e5a936e55
Takuya Hosooka <tahosook@yahoo-corp.jp>
2018-01-01 14:33:26 +0900

    Added functions sample

---
+++ functions/src/index.ts
@@ -0,0 +1,20 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/functions/write-firebase-functions
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1 // london is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+});
